e4b26fd329b5f72d80892adca013a9f8dcc76d7b
Lakshay Jain <lakshayjain@Jayas-MacBook-Pro.local>
2023-07-31 14:22:50 +0530

    several-changes

--- main.rb
+++ main.rb
@@ -1,12 +1,22 @@
 require './users.rb'
 require './database_setup.rb'
+require './tasks.rb'
+require 'date'
+
+db=DatabaseSetup.new
+db.setup_database()
 
 
-# setup_database()
 
 
-
+=begin  (For Testing Purpose Only)
 u1=User.new('chinku','chinku@gmail.com','password','user')
 u1.save()
 
+u2=User.new('lakshay','lakshayadmin@gmail.com','password','admin')
+u2.save()
 
+task1=Task.new(100,1,"feature sensor add motion also",Date.today+3,1,3,'pending');
+task1.save()
+
+=end
--- users.rb
+++ users.rb
@@ -1,18 +1,17 @@
 require './database_setup.rb'
 
 class User
-  attr_accessor :name,:email,:password,:user_type
+  attr_accessor :name, :email, :password, :user_type
 
-  def initialize(name,email,password,user_type)
+  def initialize(name, email, password, type)
     self.name=name
     self.email=email
     self.password=password
-    self.user_type=user_type
+    self.type=user_type
   end
 
-
+  db=DatabaseSetup.new
   def save 
-    connection=PG.connect(dbname: 'postgres',user: 'lakshayjain' , host: 'localhost')
-    connection.exec_params("INSERT INTO users (name,email,password,user_type) VALUES($1 , $2, $3, $4)",[self.name,self.email,self.password,self.user_type])
+    db.connection.exec_params("INSERT INTO users (name, email, password, type) VALUES($1 , $2, $3, $4)",[name, email, password, type])
   end
 end
