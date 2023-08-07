232c779a13a454de32f280d53d1785a926558eca
Lakshay Jain <lakshayjain@Jayas-MacBook-Pro.local>
2023-08-07 14:10:03 +0530

    user.rb test-case

--- spec/user.rb
+++ spec/user.rb
@@ -8,14 +8,25 @@ RSpec.describe User do
   #   allow(DatabaseConnection).to receive(:connection).and_return(fake_connection)
   # end
 
-  describe '#save' do
-    context 'when the user is successfully saved' do
-      it 'prints success message' do
-        expect(fake_connection).to receive(:exec_params).and_return(nil)
-        expect { User.new(name: 'John Doe', email: 'john@example.com', password: 'password123', type: 'user').save }
-          .to output("User Created Successfully\n").to_stdout
-      end
+  describe '#initialize' do
+    it 'assigns the attributes correctly' do
+      user = User.new(name: 'Dummy', email: 'dummy@gmail.com', password: 'password', type: 'user')
+      expect(user.name).to eq('Dummy')
+      expect(user.email).to eq('dummy@gmail.com')
+      expect(user.password).to eq('password')
+      expect(user.type).to eq('user')
+      expect(user.id).to be_nil
     end
   end
 
+
+  describe '#save' do
+    context 'when the user does not exist' do
+      it 'creates a new user and prints success message' do
+        allow(User).to receive(:find_by_email).and_return(nil)
+        user = User.new(name: 'Dummy', email: 'dummy@gmail.com', password: 'password', type: 'user')
+        expect { user.save }.to output("User Created Successfully\n").to_stdout
+      end
+    end
+  end
 end
